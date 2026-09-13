46c145ec3febbb28c4fadccbe35d096b2a46d85a
Kurasava2 <flashbladepoint2@mail.ru>
2026-09-14 02:30:57 +1000

    Load v1.4.1 period layout styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './v141.css'
 import './usability.css'
 
 createRoot(document.getElementById('root')).render(
