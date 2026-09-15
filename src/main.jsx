40ae3d81712153939cf09e9e32286f159fe0128d
Kurasava2 <flashbladepoint2@mail.ru>
2026-09-16 00:27:07 +1000

    Perf: load trip modal overrides

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import './usability.css'
 import './v150.css'
+import './tripPerformance.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode><App /></React.StrictMode>
