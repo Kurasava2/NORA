e0b14ce9329549c636e9f969fb3c0654ce8c882e
Kurasava2 <flashbladepoint2@mail.ru>
2026-09-17 01:25:12 +1000

    Stop cascading carry on trip edits

--- src/modals/trip/useTripActions.js
+++ src/modals/trip/useTripActions.js
@@ -3,7 +3,6 @@ import {
   num,
   reconcileCarryForward,
   sortTrips,
-  syncFutureVehicleCarry,
 } from '../../lib/domain.js'
 import { insertionListOrder } from '../../lib/trips.js'
 import { tripFormErrors } from './tripFormValidation.js'
@@ -88,7 +87,6 @@ export default function useTripActions({
         syncFollowingTripStart(nextStatement, tripRecord.id, vehicle)
       }
       reconcileCarryForward(nextState, nextStatement)
-      syncFutureVehicleCarry(nextState, nextStatement.vehicleId, nextPeriod)
     })
 
     notify(editingId ? 'Путёвка обновлена' : 'Путёвка добавлена')
@@ -115,7 +113,6 @@ export default function useTripActions({
         candidateTrip => candidateTrip.id !== editingId,
       )
       reconcileCarryForward(nextState, nextStatement)
-      syncFutureVehicleCarry(nextState, nextStatement.vehicleId, nextPeriod)
     })
 
     notify('Путёвка удалена')
