d0bc00caee1260ff660fb91a9f699d26b51bcd3c
Kurasava2 <flashbladepoint2@mail.ru>
2026-09-19 07:26:02 +1000

    Expose async close handshake to renderer

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -3,20 +3,29 @@ const { contextBridge, ipcRenderer } = require('electron')
 contextBridge.exposeInMainWorld('desktopAPI', {
   isElectron: true,
   loadData: () => ipcRenderer.invoke('data:load'),
-  saveData: data => ipcRenderer.invoke('data:save', data),
-  saveDataSync: data => ipcRenderer.sendSync('data:saveSync', data),
-  exportBackup: (data, suggestedName) => ipcRenderer.invoke('backup:export', { data, suggestedName }),
+  saveData: (data, options) => ipcRenderer.invoke('data:save', { data, options }),
+  exportBackup: (data, suggestedName) =>
+    ipcRenderer.invoke('backup:export', { data, suggestedName }),
   importBackup: () => ipcRenderer.invoke('backup:import'),
-  saveBinary: (bytes, suggestedName, filters) => ipcRenderer.invoke('file:saveBinary', { bytes, suggestedName, filters }),
+  saveBinary: (bytes, suggestedName, filters) =>
+    ipcRenderer.invoke('file:saveBinary', { bytes, suggestedName, filters }),
   listPrinters: () => ipcRenderer.invoke('printers:list'),
   printHtml: (html, options) => ipcRenderer.invoke('print:html', { html, options }),
   chooseTemplate: () => ipcRenderer.invoke('template:choose'),
   loadTemplate: () => ipcRenderer.invoke('template:load'),
   removeTemplate: () => ipcRenderer.invoke('template:remove'),
   getAppInfo: () => ipcRenderer.invoke('app:info'),
-  logError: (scope, message) => ipcRenderer.invoke('app:logError', { scope, message }),
+  logError: (scope, message) =>
+    ipcRenderer.invoke('app:logError', { scope, message }),
   minimizeWindow: () => ipcRenderer.invoke('window:minimize'),
   maximizeWindow: () => ipcRenderer.invoke('window:maximize'),
   closeWindow: () => ipcRenderer.invoke('window:close'),
-  isMaximized: () => ipcRenderer.invoke('window:isMaximized')
+  completeClose: () => ipcRenderer.invoke('window:completeClose'),
+  cancelClose: () => ipcRenderer.invoke('window:cancelClose'),
+  onCloseRequested: callback => {
+    const listener = () => callback()
+    ipcRenderer.on('window:close-requested', listener)
+    return () => ipcRenderer.removeListener('window:close-requested', listener)
+  },
+  isMaximized: () => ipcRenderer.invoke('window:isMaximized'),
 })
