996fd91df179fb4193bddae8d9f53211c0bb82fe
Kurasava2 <flashbladepoint2@mail.ru>
2026-09-19 07:17:47 +1000

    Add SQLite database path

--- electron/paths.cjs
+++ electron/paths.cjs
@@ -2,8 +2,15 @@ const { app } = require('electron')
 const path = require('path')
 
 const dataPath = () => path.join(app.getPath('userData'), 'gsm-data.json')
+const databasePath = () => path.join(app.getPath('userData'), 'gsm-data.sqlite')
 const templatePath = () => path.join(app.getPath('userData'), 'statement-template.xlsx')
 const templateMetaPath = () => path.join(app.getPath('userData'), 'statement-template.json')
 const errorLogPath = () => path.join(app.getPath('userData'), 'gsm-errors.log')
 
-module.exports = { dataPath, templatePath, templateMetaPath, errorLogPath }
+module.exports = {
+  dataPath,
+  databasePath,
+  templatePath,
+  templateMetaPath,
+  errorLogPath,
+}
